8e0a42a389e7a1a39b8d83188f0591f679af8241
QuakyCZ <kvaky.kratschmer@gmail.com>
2020-02-26 13:15:13 +0100

    skodf

---
+++ backend/add_seminar.php
@@ -0,0 +1,7 @@
+<?php
+    $seminarname = $_POST['seminarname'];
+    $seminardescription = $_POST['seminar']
+    $seminarroom = $_POST['seminarroom'];
+    $seminarstart = $_POST['seminarstart'];
+    $seminarend = $_POST['seminarend'];
+?>
--- backend/select_seminars.php
+++ backend/select_seminars.php
@@ -6,7 +6,7 @@ foreach($seminarsselected as &$semid){
     $_SESSION['seminars']=strval($_SESSION['seminars']).",".strval($semid);    
 }
 echo $_SESSION['seminars'];
-$stmt = "UPDATE Users SET seminars=".$_SESSION['seminars'];
+$stmt = "UPDATE Users SET seminars='".$_SESSION['seminars']."' WHERE email='".mysqli_real_escape_string($connect,$_SESSION['email'])."'";
 mysqli_query($connect, $stmt);
 
 ?>
