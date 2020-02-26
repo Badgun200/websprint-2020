6a525f02dfc78824288ef6df33ec702e0b6b1cad
QuakyCZ <kvaky.kratschmer@gmail.com>
2020-02-26 12:22:19 +0100

    jj

--- index.php
+++ index.php
@@ -6,7 +6,5 @@ if(empty($_SESSION['email'])){
 else{
     require "backend/view_seminars.php";
     require 'static/logout.html';
-    require 'backend/notification_send.php';
-    echo notification_send("kvaky.kratschmer@gmail.com","subject","text");    
 }
 ?>
