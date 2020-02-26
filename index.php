506e551bad4078ce393dad0631ea025a85617944
ofiala-a51 <temp.xanomes@volny.cz>
2020-02-26 12:35:41 +0100

    add dummy user interface

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ session_start();
 if(empty($_SESSION['email'])){
     include("static/index.html");
 }
-else{
+elseif($_SESSION['role'] === "admin"){
+    require("backend/admin_interface.php");
+    require("static/logout.html");
+}else{
     require "backend/choose_seminars.php";
     require 'static/logout.html'; 
 }
